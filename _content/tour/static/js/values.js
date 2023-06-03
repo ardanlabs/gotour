6b80ef4c8488150704abf8aaa613e0c77e65c14f
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-06-03 18:14:54 +0000

    replace debug url to ardanlabs gotour.

--- _content/tour/static/js/values.js
+++ _content/tour/static/js/values.js
@@ -108,7 +108,7 @@ value('translation', {
     'submit-feedback': 'Send feedback about this page',
 
     // GitHub issue template: update repo and messaging when translating.
-    'github-repo': 'github.com/golang/tour',
+    'github-repo': 'github.com/ardanlabs/gotour',
     'issue-title': 'tour: [REPLACE WITH SHORT DESCRIPTION]',
     'issue-message': 'Change the title above to describe your issue and add your feedback here, including code if necessary',
     'context': 'Context',
