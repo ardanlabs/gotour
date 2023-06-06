2a1c0a78bf77d1a585d61ce675c10e75bdf68658
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-06-06 12:07:26 +0000

    update embedding action cta. fix packaging nested lists.

--- _content/tour/static/js/values.js
+++ _content/tour/static/js/values.js
@@ -10,7 +10,9 @@ angular.module('tour.values', []).
 value('tableOfContents', [{
     'id': 'welcome',
     'title': 'Welcome',
-    'description': ``,
+    'description': `<p>This is material for any intermediate-level developer who has some experience with other programming languages and wants to learn Go. We believe these classes are perfect for anyone who wants a jump start in learning Go or who wants a more thorough understanding of the language and its internals.</p>
+    <p>Note: This material has been designed to be taught in a classroom environment. The code is well commented but missing some of the contextual concepts and ideas that will be covered in class.</p>
+    `,
     'lessons': [
         'welcome',
     ]
