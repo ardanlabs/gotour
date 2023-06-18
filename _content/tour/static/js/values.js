588bcf9648fca8bffcfe26bf24faf3fd658603c7
William Kennedy <bill@ardanlabs.com>
2023-06-18 14:07:13 -0400

    fixing content

--- _content/tour/static/js/values.js
+++ _content/tour/static/js/values.js
@@ -10,8 +10,7 @@ angular.module('tour.values', []).
 value('tableOfContents', [{
     'id': 'welcome',
     'title': '',
-    'description': `<p>This is material for any intermediate-level developer who has some experience with other programming languages and wants to learn Go. We believe these classes are perfect for anyone who wants a jump start in learning Go or who wants a more thorough understanding of the language and its internals.</p>
-    <p>Note: This material has been designed to be taught in a classroom environment. The code is well commented but missing some of the contextual concepts and ideas that will be covered in class.</p>
+    'description': `<p>This is material for any intermediate-level developer who has some experience with other programming languages and wants to learn Go. We believe this material is perfect for anyone who wants a jump start in learning Go or who wants a more thorough understanding of the language and its internals.</p>
     `,
     'lessons': [
         'welcome',
