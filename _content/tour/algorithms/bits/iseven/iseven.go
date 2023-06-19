18c61043829141b295f7c8b9b2ef8380697f9325
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-06-19 12:30:42 +0000

    update code example.

--- _content/tour/algorithms/bits/iseven/iseven.go
+++ _content/tour/algorithms/bits/iseven/iseven.go
@@ -1,3 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"iseven"
+)
+
+func main() {
+
+	fmt.Println(8, ":", iseven.IsEven(8))
+	fmt.Println(15, ":", iseven.IsEven(15))
+	fmt.Println(4, ":", iseven.IsEven(4))
+}
+
+
+-- go.mod --
+
+module      "play.ground"
+
+replace (
+
+    "iseven"    => ./iseven
+)
+
+-- iseven/iseven.go --
 package iseven
 
 // IsEven checks is an integer is even.
