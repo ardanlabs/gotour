7e246b21b8e3430a413ffde7ab242cf613ee5f33
leandrorichardardan <leandro.toledo@ardanlabs.com>
2023-09-19 09:54:03 -0300

    added russian content test

---
+++ _content/tour/rus/channels/example10.go
@@ -0,0 +1,53 @@
+//go:build OMIT
+
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// This sample program demonstrates the channel cancellation channel pattern.
+package main
+
+import (
+	"context"
+	"net/http"
+)
+
+func main() {
+	stop := make(chan struct{})
+
+	channelCancellation(stop)
+}
+
+// channelCancellation shows how you can take an existing channel being
+// used for cancellation and convert that into using a context where
+// a context is needed.
+func channelCancellation(stop <-chan struct{}) {
+
+	// Create a cancel context for handling the stop signal.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// If a signal is received on the stop channel, cancel the
+	// context. This will propagate the cancel into the p.Run
+	// function below.
+	go func() {
+		select {
+		case <-stop:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	// Imagine a function that is performing an I/O operation that is
+	// cancellable.
+	func(ctx context.Context) error {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.ardanlabs.com/blog/index.xml", nil)
+		if err != nil {
+			return err
+		}
+		_, err = http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		return nil
+	}(ctx)
+}
