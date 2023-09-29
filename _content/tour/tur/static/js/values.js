88346a2ebba7af4389d62ea3d92a0279b7d1a404
Yavuz Günay <yvz5@users.noreply.github.com>
2023-09-29 16:59:17 +0300

    turkish translation

--- _content/tour/tur/static/js/values.js
+++ _content/tour/tur/static/js/values.js
@@ -6,158 +6,158 @@
 
 angular.module('tour.values', []).
 
-// List of modules with description and lessons in it.
-value('tableOfContents', [{
-    'id': 'welcome',
-    'title': '',
-    'description': `<p>Bu, diğer programlama dilleri konusunda biraz deneyimi olan ve Go'yu öğrenmek isteyen orta düzey geliştiricilere yönelik bir materyaldir. Bu materyalin Go öğrenmeye hızlı bir başlangıç yapmak isteyen veya dili ve dilin iç yapısını daha kapsamlı anlamak isteyen herkes için mükemmel olduğuna inanıyoruz.</p>
+    // List of modules with description and lessons in it.
+    value('tableOfContents', [{
+        'id': 'welcome',
+        'title': '',
+        'description': `<p>Bu, diğer programlama dilleri konusunda biraz deneyimi olan ve Go'yu öğrenmek isteyen orta düzey geliştiricilere yönelik bir materyaldir. Bu materyalin Go öğrenmeye hızlı bir başlangıç yapmak isteyen veya dili ve dilin iç yapısını daha kapsamlı anlamak isteyen herkes için mükemmel olduğuna inanıyoruz.</p>
     <p><b>Bir örneği ilk kez çalıştırdığınızda tamamlanması biraz zaman alabilir. Bunun nedeni tur hizmetinin çalışmıyor olmasıdır. Lütfen tamamlanması için biraz zaman verin.</b></p>
     `,
-    'lessons': [
-        'welcome',
-    ]
-}, {
-    'id': 'language-mechanics',
-    'title': 'Language Mechanics',
-    'description': `
-        <p>This material covers all the language syntax, idioms, implementation and specification of the language. Once you are done with this material you will understand the mechanics of the language and mechanical sympathies the language has for both the hardware and operating system.</p>
+        'lessons': [
+            'welcome',
+        ]
+    }, {
+        'id': 'language-mechanics',
+        'title': 'Dil Mekanikleri',
+        'description': `
+        <p>Bu materyal, dilin tüm sözdizimi, deyimleri, uygulaması ve dilin hem donanım hem de işletim sistemi için mekanik sempatisini kapsar. Bu materyali bitirdiğinizde, dilin mekaniklerini anlayacak ve dilin hem donanım hem de işletim sistemi için mekanik sempatisini kavrayacaksınız.</p>
         `,
-    'lessons': [
-        'variables',
-        'struct-types', 
-        'pointers', 
-        'constants',
-        'functions',
-        'arrays',
-        'slices',
-        'maps',
-        'methods',
-        'interfaces',
-        'embedding',
-        'exporting',
-    ]
-}, {
-    'id': 'composition-interfaces',
-    'title': 'Composition and Interfaces',
-    'description': `
-        <p>This material covers the practical things you need to know about composition and interfaces.</p>
+        'lessons': [
+            'variables',
+            'struct-types',
+            'pointers',
+            'constants',
+            'functions',
+            'arrays',
+            'slices',
+            'maps',
+            'methods',
+            'interfaces',
+            'embedding',
+            'exporting',
+        ]
+    }, {
+        'id': 'composition-interfaces',
+        'title': 'Kompozisyon ve Arayüzler',
+        'description': `
+        <p>Bu materyal, kompozisyon ve arayüzler hakkında bilmeniz gereken pratik konuları kapsar.</p>
         `,
-    'lessons': [
-        'composition-grouping',
-        'composition-assertions',
-        'composition-pollution',
-        'composition-mocking',
-        'composition-decoupling',
-        'error-handling',
-    ]
-},{
-    'id': 'concurrency',
-    'title': 'Concurrency',
-    'description': `
-        <p>This material covers all the concurrency aspects of the language. Once you are done with this material you will understand the concurrent mechanics of the language and mechanical sympathies the language has for both the hardware and operating system as it related to concurrency.</p>
+        'lessons': [
+            'composition-grouping',
+            'composition-assertions',
+            'composition-pollution',
+            'composition-mocking',
+            'composition-decoupling',
+            'error-handling',
+        ]
+    }, {
+        'id': 'concurrency',
+        'title': 'Paralellik',
+        'description': `
+        <p>Bu materyal, dilin tüm paralellik yönlerini kapsar. Bu materyali tamamladığınızda, dilin paralel mekaniğini anlayacak ve dilin donanım ve işletim sistemiyle ilgili olarak paralellikle ilgili mekanik duyarlılıklarını kavrayacaksınız.</p>
        `,
-    'lessons': [
-        'goroutines',
-        'data_race',
-        'context',
-        'channels',
-    ]
-},{
-    'id': 'generics',
-    'title': 'Generics',
-    'description': `
-        <p>This material covers all the generics aspects of the language. Generics is about providing the ability to write concrete polymorphic functions with the support of type parameter lists.</p>
+        'lessons': [
+            'goroutines',
+            'data_race',
+            'context',
+            'channels',
+        ]
+    }, {
+        'id': 'generics',
+        'title': 'Jenerikler',
+        'description': `
+        <p>Bu materyal, dilin tüm genel (generic) yönlerini kapsar. Generics, tip parametre listelerinin desteğiyle somut polimorfik fonksiyonları yazma yeteneği hakkında bir konsepttir.</p>
        `,
-    'lessons': [
-        'generics-basics',
-        'generics-underlying-types',
-        'generics-struct-types',
-        'generics-behavior-constraints',
-        'generics-type-constraints',
-        'generics-multi-type-params',
-        'generics-slice-constraints',
-        'generics-channels',
-        'generics-hash-table',
-    ]
-},{
-    'id': 'algorithms',
-    'title': 'Algorithms',
-    'description': `
-        <p>This material provides Go code implementing a common and fun set of algorithms.</p>
+        'lessons': [
+            'generics-basics',
+            'generics-underlying-types',
+            'generics-struct-types',
+            'generics-behavior-constraints',
+            'generics-type-constraints',
+            'generics-multi-type-params',
+            'generics-slice-constraints',
+            'generics-channels',
+            'generics-hash-table',
+        ]
+    }, {
+        'id': 'algorithms',
+        'title': 'Algoritmalar',
+        'description': `
+        <p>Bu materyal, ortak ve eğlenceli bir dizi algoritmayı uygulayan Go kodu sağlar.</p>
        `,
-    'lessons': [
-        'algorithms-bits-seven',
-        'algorithms-strings',
-        'algorithms-numbers',
-        'algorithms-slices',
-        'algorithms-sorting',
-        'algorithms-data',
-        'algorithms-searches',
-        'algorithms-fun',
-    ]
-}]).
-// translation
-value('translation', {
-    'off': 'off',
-    'on': 'on',
-    'syntax': 'Syntax-Highlighting',
-    'lineno': 'Line-Numbers',
-    'reset': 'Reset Slide',
-    'format': 'Format Source Code',
-    'kill': 'Kill Program',
-    'run': 'Run',
-    'compile': 'Compile and Run',
-    'more': 'Options',
-    'toc': 'Table of Contents',
-    'prev': 'Previous',
-    'next': 'Next',
-    'waiting': 'Waiting for remote server...',
-    'errcomm': 'Error communicating with remote server.',
-    'submit-feedback': 'Send feedback about this page',
-    'search': 'Search for content',
+        'lessons': [
+            'algorithms-bits-seven',
+            'algorithms-strings',
+            'algorithms-numbers',
+            'algorithms-slices',
+            'algorithms-sorting',
+            'algorithms-data',
+            'algorithms-searches',
+            'algorithms-fun',
+        ]
+    }]).
+    // translation
+    value('translation', {
+        'off': 'kapalı',
+        'on': 'açık',
+        'syntax': 'Sözdizimi-Vurgulama',
+        'lineno': 'Satır-Numaraları',
+        'reset': 'Slaytı Sıfırla',
+        'format': 'Kaynak Kodu Biçimlendir',
+        'kill': 'Programı Sonlandır',
+        'run': 'Çalıştır',
+        'compile': 'Derle ve Çalıştır',
+        'more': 'Seçenekler',
+        'toc': 'İçindekiler',
+        'prev': 'Önceki',
+        'next': 'Sonraki',
+        'waiting': 'Uzak sunucu bekleniyor...',
+        'errcomm': 'Uzak sunucu ile iletişim hatası.',
+        'submit-feedback': 'Bu sayfa hakkında geri bildirim gönder',
+        'search': 'İçerik ara',
 
-    // GitHub issue template: update repo and messaging when translating.
-    'github-repo': 'github.com/ardanlabs/gotour',
-    'issue-title': 'tour: [REPLACE WITH SHORT DESCRIPTION]',
-    'issue-message': 'Change the title above to describe your issue and add your feedback here, including code if necessary',
-    'context': 'Context',
-}).
+        // GitHub issue template: update repo and messaging when translating.
+        'github-repo': 'github.com/ardanlabs/gotour',
+        'issue-title': 'tour: [REPLACE WITH SHORT DESCRIPTION]',
+        'issue-message': 'Change the title above to describe your issue and add your feedback here, including code if necessary',
+        'context': 'Context',
+    }).
 
-// Config for codemirror plugin
-value('ui.config', {
-    codemirror: {
-        mode: 'text/x-go',
-        matchBrackets: true,
-        lineNumbers: true,
-        autofocus: true,
-        indentWithTabs: true,
-        indentUnit: 4,
-        tabSize: 4,
-        lineWrapping: true,
-        extraKeys: {
-            'Shift-Enter': function() {
-                $('#run').click();
+    // Config for codemirror plugin
+    value('ui.config', {
+        codemirror: {
+            mode: 'text/x-go',
+            matchBrackets: true,
+            lineNumbers: true,
+            autofocus: true,
+            indentWithTabs: true,
+            indentUnit: 4,
+            tabSize: 4,
+            lineWrapping: true,
+            extraKeys: {
+                'Shift-Enter': function () {
+                    $('#run').click();
+                },
+                'Ctrl-Enter': function () {
+                    $('#format').click();
+                },
+                'PageDown': function () {
+                    return false;
+                },
+                'PageUp': function () {
+                    return false;
+                },
             },
-            'Ctrl-Enter': function() {
-                $('#format').click();
-            },
-            'PageDown': function() {
-                return false;
-            },
-            'PageUp': function() {
-                return false;
-            },
-        },
-        // TODO: is there a better way to do this?
-        // AngularJS values can't depend on factories.
-        onChange: function() {
-            if (window.codeChanged !== null) window.codeChanged();
+            // TODO: is there a better way to do this?
+            // AngularJS values can't depend on factories.
+            onChange: function () {
+                if (window.codeChanged !== null) window.codeChanged();
+            }
         }
-    }
-}).
+    }).
 
-// mapping from the old paths (#42) to the new organization.
-// The values have been generated with the map.sh script in the tools directory.
-value('mapping', {
-    '#1': '/variables/1', // Variables
-});
+    // mapping from the old paths (#42) to the new organization.
+    // The values have been generated with the map.sh script in the tools directory.
+    value('mapping', {
+        '#1': '/variables/1', // Variables
+    });
