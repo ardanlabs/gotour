24b6434412950625bc058a0c2c24bd5171389c26
drycog <nohofupetu@gmail.com>
2023-10-10 08:24:50 +0300

    Repaired greek fork

---
+++ _content/tour/grc/channels/exercise2.go
@@ -0,0 +1,41 @@
+//go:build OMIT
+
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Write a program that uses a fan out pattern to generate 100 random numbers
+// concurrently. Have each goroutine generate a single random number and return
+// that number to the main goroutine over a buffered channel. Set the size of
+// the buffered channel so no send ever blocks. Don't allocate more capacity
+// than you need. Have the main goroutine store each random number it receives
+// in a slice. Print the slice values then terminate the program.
+package main
+
+// Add imports.
+
+// Declare constant for number of goroutines.
+
+func init() {
+	// Seed the random number generator.
+}
+
+func main() {
+
+	// Create the buffered channel with room for
+	// each goroutine to be created.
+
+	// Iterate and launch each goroutine.
+	{
+
+		// Create an anonymous function for each goroutine that
+		// generates a random number and sends it on the channel.
+	}
+
+	// Create a variable to be used to track received messages.
+	// Set the value to the number of goroutines created.
+
+	// Iterate receiving each value until they are all received.
+	// Store them in a slice of ints.
+
+	// Print the values in our slice.
+}
---
+++ _content/tour/grc/welcome/hello.go
@@ -0,0 +1,9 @@
+//go:build OMIT
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
