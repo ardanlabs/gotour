1c0835c461dcdffe1d4a46edcd6dd458a713b88d
tty2 <tty2.rs@gmail.com>
2024-01-17 18:34:51 +0000

    extend maps article

---
+++ _content/tour/eng/maps/example8.go
@@ -0,0 +1,25 @@
+//go:build OMIT
+
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Sample program to show how nil maps works with set operation.
+package main
+
+import "fmt"
+
+// user represents someone using the program.
+type user struct {
+	name    string
+	surname string
+}
+
+func main() {
+	var users map[string]user
+
+	value, exists := users["Roy"] // safe operation
+	fmt.Println(value)            // empty struct
+	fmt.Println(exists)           // false
+
+	users["Roy"] = user{"Rob", "Roy"} // panic: assignment to entry in nil map
+}
