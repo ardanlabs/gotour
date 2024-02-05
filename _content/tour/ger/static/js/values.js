9dc011737257c5368f3ffa519614c743cc768fe3
doemsche <doemsche@gmx.ch>
2024-02-05 11:46:28 +0100

    update static content

--- _content/tour/ger/static/js/values.js
+++ _content/tour/ger/static/js/values.js
@@ -2,162 +2,161 @@
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
-'use strict';
+"use strict";
 
-angular.module('tour.values', []).
-
-// List of modules with description and lessons in it.
-value('tableOfContents', [{
-    'id': 'welcome',
-    'title': '',
-    'description': `<p>Dies ist Material für jeden Entwickler mittlerer Stufe, der Erfahrung mit anderen Programmiersprachen hat und Go lernen möchte. Wir glauben, dass dieses Material perfekt für alle ist, die einen schnellen Einstieg in das Erlernen von Go wünschen oder ein tieferes Verständnis der Sprache und ihrer Interna wünschen.</p>
+angular
+  .module("tour.values", [])
+  // List of modules with description and lessons in it.
+  .value("tableOfContents", [
+    {
+      id: "welcome",
+      title: "",
+      description: `<p>Dies ist Material für jeden Entwickler mittlerer Stufe, der Erfahrung mit anderen Programmiersprachen hat und Go lernen möchte. Wir glauben, dass dieses Material perfekt für alle ist, die einen schnellen Einstieg in das Erlernen von Go wünschen oder ein tieferes Verständnis der Sprache und ihrer Interna wünschen.</p>
     <p><b>Wenn Sie ein Beispiel zum ersten Mal ausführen, kann es einige Zeit dauern, bis es fertig ist. Dies liegt daran, dass der Tourservice möglicherweise nicht ausgeführt wird. Bitte geben Sie etwas Zeit für die Fertigstellung.</b></p>
     `,
-    'lessons': [
-        'welcome',
-    ]
-}, {
-    'id': 'language-mechanics',
-    'title': 'Language Mechanics',
-    'description': `
-        <p>This material covers all the language syntax, idioms, implementation and specification of the language. Once you are done with this material you will understand the mechanics of the language and mechanical sympathies the language has for both the hardware and operating system.</p>
+      lessons: ["welcome"],
+    },
+    {
+      id: "language-mechanics",
+      title: "Language Mechanics",
+      description: `
+        <p>Dieses Material behandelt die gesamte Sprachsyntax, Idiome, Implementierung und Spezifikation der Sprache. Wenn Sie mit diesem Material fertig sind, werden Sie die Mechanik der Sprache und die mechanischen sympathies verstehen, die die Sprache sowohl für die Hardware als auch für das Betriebssystem hat.</p>
         `,
-    'lessons': [
-        'variables',
-        'struct-types', 
-        'pointers', 
-        'constants',
-        'functions',
-        'arrays',
-        'slices',
-        'maps',
-        'methods',
-        'interfaces',
-        'embedding',
-        'exporting',
-    ]
-}, {
-    'id': 'composition-interfaces',
-    'title': 'Composition and Interfaces',
-    'description': `
-        <p>This material covers the practical things you need to know about composition and interfaces.</p>
+      lessons: [
+        "variables",
+        "struct-types",
+        "pointers",
+        "constants",
+        "functions",
+        "arrays",
+        "slices",
+        "maps",
+        "methods",
+        "interfaces",
+        "embedding",
+        "exporting",
+      ],
+    },
+    {
+      id: "composition-interfaces",
+      title: "Composition and Interfaces",
+      description: `
+        <p>Dieses Material behandelt die praktischen Dinge, die Sie über Komposition und Schnittstellen wissen müssen.</p>
         `,
-    'lessons': [
-        'composition-grouping',
-        'composition-assertions',
-        'composition-pollution',
-        'composition-mocking',
-        'composition-decoupling',
-        'error-handling',
-    ]
-},{
-    'id': 'concurrency',
-    'title': 'Concurrency',
-    'description': `
-        <p>This material covers all the concurrency aspects of the language. Once you are done with this material you will understand the concurrent mechanics of the language and mechanical sympathies the language has for both the hardware and operating system as it related to concurrency.</p>
+      lessons: [
+        "composition-grouping",
+        "composition-assertions",
+        "composition-pollution",
+        "composition-mocking",
+        "composition-decoupling",
+        "error-handling",
+      ],
+    },
+    {
+      id: "concurrency",
+      title: "Concurrency",
+      description: `
+        <p>Dieses Material deckt alle Gleichzeitigkeitsaspekte der Sprache ab. Wenn Sie mit diesem Material fertig sind, werden Sie die Gleichzeitigkeitsmechanik der Sprache und die mechanischen Sympathien verstehen, die die Sprache sowohl für die Hardware als auch für das Betriebssystem hat, wenn es um Gleichzeitigkeit geht.</p>
        `,
-    'lessons': [
-        'goroutines',
-        'data_race',
-        'context',
-        'channels',
-    ]
-},{
-    'id': 'generics',
-    'title': 'Generics',
-    'description': `
-        <p>This material covers all the generics aspects of the language. Generics is about providing the ability to write concrete polymorphic functions with the support of type parameter lists.</p>
+      lessons: ["goroutines", "data_race", "context", "channels"],
+    },
+    {
+      id: "generics",
+      title: "Generics",
+      description: `
+        <p>Dieses Material behandelt alle generischen Aspekte der Sprache. Bei den Generika geht es um die Möglichkeit, konkrete polymorphe Funktionen mit Unterstützung von Typparameterlisten zu schreiben.</p>
        `,
-    'lessons': [
-        'generics-basics',
-        'generics-underlying-types',
-        'generics-struct-types',
-        'generics-behavior-constraints',
-        'generics-type-constraints',
-        'generics-multi-type-params',
-        'generics-slice-constraints',
-        'generics-channels',
-        'generics-hash-table',
-    ]
-},{
-    'id': 'algorithms',
-    'title': 'Algorithms',
-    'description': `
-        <p>This material provides Go code implementing a common and fun set of algorithms.</p>
+      lessons: [
+        "generics-basics",
+        "generics-underlying-types",
+        "generics-struct-types",
+        "generics-behavior-constraints",
+        "generics-type-constraints",
+        "generics-multi-type-params",
+        "generics-slice-constraints",
+        "generics-channels",
+        "generics-hash-table",
+      ],
+    },
+    {
+      id: "algorithms",
+      title: "Algorithms",
+      description: `
+        <p>Dieses Material bietet Go-Code, der eine Reihe von gängigen und unterhaltsamen Algorithmen implementiert.</p>
        `,
-    'lessons': [
-        'algorithms-bits-seven',
-        'algorithms-strings',
-        'algorithms-numbers',
-        'algorithms-slices',
-        'algorithms-sorting',
-        'algorithms-data',
-        'algorithms-searches',
-        'algorithms-fun',
-    ]
-}]).
-// translation
-value('translation', {
-    'off': 'off',
-    'on': 'on',
-    'syntax': 'Syntax-Highlighting',
-    'lineno': 'Line-Numbers',
-    'reset': 'Reset Slide',
-    'format': 'Format Source Code',
-    'kill': 'Kill Program',
-    'run': 'Run',
-    'compile': 'Compile and Run',
-    'more': 'Options',
-    'toc': 'Table of Contents',
-    'prev': 'Previous',
-    'next': 'Next',
-    'waiting': 'Waiting for remote server...',
-    'errcomm': 'Error communicating with remote server.',
-    'submit-feedback': 'Send feedback about this page',
-    'search': 'Search for content',
+      lessons: [
+        "algorithms-bits-seven",
+        "algorithms-strings",
+        "algorithms-numbers",
+        "algorithms-slices",
+        "algorithms-sorting",
+        "algorithms-data",
+        "algorithms-searches",
+        "algorithms-fun",
+      ],
+    },
+  ])
+  // translation
+  .value("translation", {
+    off: "off",
+    on: "on",
+    syntax: "Syntax-Highlighting",
+    lineno: "Line-Numbers",
+    reset: "Reset Slide",
+    format: "Format Source Code",
+    kill: "Kill Program",
+    run: "Run",
+    compile: "Compile and Run",
+    more: "Options",
+    toc: "Table of Contents",
+    prev: "Previous",
+    next: "Next",
+    waiting: "Waiting for remote server...",
+    errcomm: "Error communicating with remote server.",
+    "submit-feedback": "Send feedback about this page",
+    search: "Search for content",
 
     // GitHub issue template: update repo and messaging when translating.
-    'github-repo': 'github.com/ardanlabs/gotour',
-    'issue-title': 'tour: [REPLACE WITH SHORT DESCRIPTION]',
-    'issue-message': 'Change the title above to describe your issue and add your feedback here, including code if necessary',
-    'context': 'Context',
-}).
-
-// Config for codemirror plugin
-value('ui.config', {
+    "github-repo": "github.com/ardanlabs/gotour",
+    "issue-title": "tour: [REPLACE WITH SHORT DESCRIPTION]",
+    "issue-message":
+      "Change the title above to describe your issue and add your feedback here, including code if necessary",
+    context: "Context",
+  })
+  // Config for codemirror plugin
+  .value("ui.config", {
     codemirror: {
-        mode: 'text/x-go',
-        matchBrackets: true,
-        lineNumbers: true,
-        autofocus: true,
-        indentWithTabs: true,
-        indentUnit: 4,
-        tabSize: 4,
-        lineWrapping: true,
-        extraKeys: {
-            'Shift-Enter': function() {
-                $('#run').click();
-            },
-            'Ctrl-Enter': function() {
-                $('#format').click();
-            },
-            'PageDown': function() {
-                return false;
-            },
-            'PageUp': function() {
-                return false;
-            },
+      mode: "text/x-go",
+      matchBrackets: true,
+      lineNumbers: true,
+      autofocus: true,
+      indentWithTabs: true,
+      indentUnit: 4,
+      tabSize: 4,
+      lineWrapping: true,
+      extraKeys: {
+        "Shift-Enter": function () {
+          $("#run").click();
         },
-        // TODO: is there a better way to do this?
-        // AngularJS values can't depend on factories.
-        onChange: function() {
-            if (window.codeChanged !== null) window.codeChanged();
-        }
-    }
-}).
-
-// mapping from the old paths (#42) to the new organization.
-// The values have been generated with the map.sh script in the tools directory.
-value('mapping', {
-    '#1': '/variables/1', // Variables
-});
+        "Ctrl-Enter": function () {
+          $("#format").click();
+        },
+        PageDown: function () {
+          return false;
+        },
+        PageUp: function () {
+          return false;
+        },
+      },
+      // TODO: is there a better way to do this?
+      // AngularJS values can't depend on factories.
+      onChange: function () {
+        if (window.codeChanged !== null) window.codeChanged();
+      },
+    },
+  })
+  // mapping from the old paths (#42) to the new organization.
+  // The values have been generated with the map.sh script in the tools directory.
+  .value("mapping", {
+    "#1": "/variables/1", // Variables
+  });
